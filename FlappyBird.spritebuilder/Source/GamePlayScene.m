622a8a132c5ffd1923a071f0e108e8821c41a699
Ata Ul Haq <ata@Atas-MBP.home>
2014-10-20 20:29:19 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,12 +10,14 @@
     [physicsNode addChild:character];
 }
 
--(void)update:(CCTime)delta
+
+
+- (void)update:(CCTime)delta
 {
     // put update code here
 }
 
-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
 }
