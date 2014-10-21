94968286e604dbec5e2d2517525d07cee177064c
Ata Ul Haq <ata@Atas-MBP.home>
2014-10-20 20:23:00 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,7 +15,7 @@
     // put update code here
 }
 
-(void)touchBegin:(UITouch *)touch withEvent:(UIEvent *)event {
+(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
 }
