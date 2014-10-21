54cf1cf4f10d88c78b51547f2b60bad3fc4d70b5
Ata Ul Haq <ata@Atas-MBP.home>
2014-10-20 20:35:30 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -21,6 +21,15 @@
     // this will be run every frame.
     // delta is the time that has elapsed since the last time it was run.
     // This is usually 1/60, but can be bigger if the game slows down
+    
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f)
+    {
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0f
+    }
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
